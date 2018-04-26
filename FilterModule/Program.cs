d57b2c92b73cfb45393ae022edf25585de7ed3f9
Ian Bennett <ianbe@microsoft.com>
2018-04-26 19:30:53 -0400

    Module updates, can't set reported properties...

---
+++ FilterModule/Program.cs
@@ -0,0 +1,155 @@
+namespace FilterModule
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Runtime.Loader;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+
+    class Program
+    {
+        static int counter;
+
+        static void Main(string[] args)
+        {
+            // The Edge runtime gives us the connection string we need -- it is injected as an environment variable
+            string connectionString = Environment.GetEnvironmentVariable("EdgeHubConnectionString");
+
+            // Cert verification is not yet fully functional when using Windows OS for the container
+            bool bypassCertVerification = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            if (!bypassCertVerification) InstallCert();
+            Init(connectionString, bypassCertVerification).Wait();
+
+            // Wait until the app unloads or is cancelled
+            var cts = new CancellationTokenSource();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+            WhenCancelled(cts.Token).Wait();
+        }
+
+        /// <summary>
+        /// Handles cleanup operations when app is cancelled or unloads
+        /// </summary>
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Add certificate in local cert store for use by client for secure connection to IoT Edge runtime
+        /// </summary>
+        static void InstallCert()
+        {
+            string certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
+            if (string.IsNullOrWhiteSpace(certPath))
+            {
+                // We cannot proceed further without a proper cert file
+                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
+                throw new InvalidOperationException("Missing path to certificate file.");
+            }
+            else if (!File.Exists(certPath))
+            {
+                // We cannot proceed further without a proper cert file
+                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
+                throw new InvalidOperationException("Missing certificate file.");
+            }
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(certPath)));
+            Console.WriteLine("Added Cert: " + certPath);
+            store.Close();
+        }
+
+
+        /// <summary>
+        /// Initializes the DeviceClient and sets up the callback to receive
+        /// messages containing temperature information
+        /// </summary>
+        static async Task Init(string connectionString, bool bypassCertVerification = false)
+        {
+            Console.WriteLine("Connection String {0}", connectionString);
+
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            // During dev you might want to bypass the cert verification. It is highly recommended to verify certs systematically in production
+            if (bypassCertVerification)
+            {
+                mqttSetting.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            }
+            ITransportSettings[] settings = { mqttSetting };
+
+            // Open a connection to the Edge runtime
+            DeviceClient ioTHubModuleClient = DeviceClient.CreateFromConnectionString(connectionString, settings);
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized fool!");
+
+            // Register callback to be called when a message is received by the module
+            //await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+            
+            Console.WriteLine("Starting timer");
+
+            var timer = new System.Timers.Timer(3000);
+            timer.Elapsed += async (Object source, System.Timers.ElapsedEventArgs e) => {
+                Console.WriteLine("Timer Elapsed at: {0}", e.SignalTime);
+                /*using(var reader = new System.IO.StreamReader("/var/log/messages")) {                    
+                    var deviceTwin = await ioTHubModuleClient.GetTwinAsync();
+                    var deviceTwinCollection = moduleTwin.Properties.Reported;
+                    deviceTwinCollection["logs"] = await reader.ReadToEndAsync();
+                    await ioTHubModuleClient.UpdateReportedPropertiesAsync(deviceTwinCollection);
+                }*/
+                  
+                // DOES NOT WORK, DEVUCE TWIN PROPERTY NOT UPDATED!?!  
+                // Replace with event hub or blob storage ?
+                var deviceTwin = await ioTHubModuleClient.GetTwinAsync();
+                var deviceTwinCollection = deviceTwin.Properties.Reported;
+                deviceTwinCollection["logs"] = string.Format("Timer Elapsed at: {0}", e.SignalTime);
+                await ioTHubModuleClient.UpdateReportedPropertiesAsync(deviceTwinCollection);
+            };
+            timer.Start();
+        }       
+        
+
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+        }
+
+        /// <summary>
+        /// This method is called whenever the module is sent a message from the EdgeHub. 
+        /// It just pipe the messages without any change.
+        /// It prints all the incoming messages.
+        /// </summary>
+        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        {
+            int counterValue = Interlocked.Increment(ref counter);
+
+            var deviceClient = userContext as DeviceClient;
+            if (deviceClient == null)
+            {
+                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+            }
+
+            byte[] messageBytes = message.GetBytes();
+            string messageString = Encoding.UTF8.GetString(messageBytes);
+            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+
+            if (!string.IsNullOrEmpty(messageString))
+            {
+                var pipeMessage = new Message(messageBytes);
+                foreach (var prop in message.Properties)
+                {
+                    pipeMessage.Properties.Add(prop.Key, prop.Value);
+                }
+                await deviceClient.SendEventAsync("output1", pipeMessage);
+                Console.WriteLine("Received message sent");
+            }
+            return MessageResponse.Completed;
+        }
+    }
+}
